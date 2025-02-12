367c8e86199c407f4db662a453935606a9ba385f
KojiOchiai <12869331+KojiOchiai@users.noreply.github.com>
2025-02-12 13:42:13 +0900

    format: format hello.py

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ class ResultMessage(BaseModel):
     message: str = Field(description="The message to be sent to the user")
     is_request: bool = Field(description="Whether the message is a request for user")
 
+
 agent = Agent(
     model="openai:gpt-4o",
     result_type=ResultMessage,
@@ -16,10 +17,12 @@ agent = Agent(
     ),
 )
 
+
 @agent.tool_plain
 def sum_tool(values: list[float]) -> float:
     print(values)
     return sum(values)
+
 
 def main():
     questions = [
@@ -28,7 +31,7 @@ def main():
         "what is my name?",
         "what is my favorite game?",
         "what is sum of 1, 2, 3, 4, 5?",
-        "what is 1243 + 44"
+        "what is 1243 + 44",
     ]
     message_history = []
     for question in questions:
